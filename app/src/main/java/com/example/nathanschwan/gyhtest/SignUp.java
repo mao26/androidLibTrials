4432e642f654edca771aff0dd21d47a91c9544a4
Nathan Schwan <nathan.schwan.94@gmail.com>
2016-10-18 18:08:15 -0400

    simple read write implementations for AWS service Dynamo DB.

--- app/src/main/java/com/example/nathanschwan/gyhtest/SignUp.java
+++ app/src/main/java/com/example/nathanschwan/gyhtest/SignUp.java
@@ -17,21 +17,41 @@ import butterknife.OnClick;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.mobile.AWSMobileClient;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedScanList;
 import com.amazonaws.models.nosql.FitnessLevelsDO;
 import com.amazonaws.models.nosql.UsersTESTDO;
 
 import com.amazonaws.models.nosql.UserDO;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+
+// import the CognitoCredentialsProvider object from the auth package
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+
+
+
 
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import android.util.Log;
+
+import java.util.List;
+import java.util.Map;
+
 import rx.Subscriber;
 
 
 
 
+
 public class SignUp extends AppCompatActivity {
+
 
     private final static String LOG_TAG = Application.class.getSimpleName();
 
@@ -44,7 +64,25 @@ public class SignUp extends AppCompatActivity {
 
 
     @OnClick(R.id.showusers)
+    protected void onShowUsersClick(){
+        Observable<Boolean> showtask = Observable.fromCallable(()->showData());
+        Thread setData = new Thread(){
+                int attempts = 0;
+                public void run(){
+                    boolean answer = showData();
+                    attempts += 1;
+                    if (!answer && attempts < 5){
+                        run();
+                    }
+                    Log.e(LOG_TAG, "INSERTED DATA SUCCESSFULLY");
+                    return;
 
+                }
+        };
+        setData.start();
+
+
+    }
 
     @OnClick(R.id.submit)
     protected void onClick() {
@@ -111,17 +149,27 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
-
     }
 
     public boolean insertData(String name, String password) {
-        // Fetch the default configured DynamoDB ObjectMapper
+        // Initialize the Amazon Cognito credentials provider
+//        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+//                this.getApplicationContext(),
+//                "us-east-1:ce4c9743-c8b5-440e-bf65-782a6f021a66", // Identity Pool ID
+//                Regions.US_EAST_1 // Region
+//        );
+//
+//
+//        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient();
+//        DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+
         final DynamoDBMapper dynamoDBMapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();
+
         final UsersTESTDO note = new UsersTESTDO(); // Initialize the Notes Object
 
         note.setName(name);
         note.setPassword(password);
-        note.setUserId("NOTUUID");
+        note.setUserId("NOTUUID1");
 //
 //        note.setPushup((double) 20);
 //        note.setSitups((double) 25);
@@ -143,5 +191,40 @@ public class SignUp extends AppCompatActivity {
         return true;
 
 
+    }
+
+    protected boolean showData(){
+        // Initialize the Amazon Cognito credentials provider
+        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                this.getApplicationContext(),
+                "us-east-1:ce4c9743-c8b5-440e-bf65-782a6f021a66", // Identity Pool ID
+                Regions.US_EAST_1 // Region
+        );
+
+
+        AmazonDynamoDBClient ddbClient = new AmazonDynamoDBClient(credentialsProvider);
+        DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+
+
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                //DynamoDB calls go here
+                try {
+                    DynamoDBScanExpression expression = new DynamoDBScanExpression();
+                    PaginatedScanList<UsersTESTDO> list = mapper.scan(UsersTESTDO.class, expression);
+                    for (UsersTESTDO item: list) {
+                        Log.e(LOG_TAG, item.getName() + item.getPassword());
+                    }
+                }
+                catch(Exception e){
+                    Log.e(LOG_TAG, e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread mythread = new Thread(runnable);
+        mythread.start();
+        return true;
     }
 }
